6c1d465066ae3930eb9d1eca85e0d0d1a6818a16
Matthieu-Furvent <ze.karel@free.fr>
2019-01-23 14:35:33 +0100

    Bug fix with search-id-pipe

--- src/app/search-id-filter.pipe.ts
+++ src/app/search-id-filter.pipe.ts
@@ -10,14 +10,14 @@ export class SearchIdFilterPipe implements PipeTransform {
     // If no search term
     if (term === undefined) return pokename;
 
-    console.log("term before toString: " + term);
     term = term.toString();
-    console.log("term after toString: " + term);
+    if (term === "0") return pokename;
     // To match with pokemon's ID, we must add the good numbe of '0'. If just one digit, add two '0', if two digits only '0', and for more nothing to add.
     term = (term.length === 1) ? ('00' + term)
          : (term.length === 2) ? ('0' + term)
          : term;
-    console.log("term after modif toString: " + term);
+
+    // Je ne comprends pas entièrement les instructions suivantes
     return pokename.filter(function (thisPokemon) {
       return thisPokemon.id.toLowerCase().includes(term.toLowerCase());
     });
