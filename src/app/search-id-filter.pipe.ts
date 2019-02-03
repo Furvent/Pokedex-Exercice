78f80b13fde7da950567569c7a0728e7f0f61bb3
Furvent <ze.karel@free.fr>
2019-02-03 16:35:11 +0100

    Fix bug on search by id pipe, handle a case where terme is null

--- src/app/search-id-filter.pipe.ts
+++ src/app/search-id-filter.pipe.ts
@@ -8,10 +8,12 @@ export class SearchIdFilterPipe implements PipeTransform {
   transform(pokemon: any, term: any): any {
 
     // If no search term
+    if (term === null) return pokemon;
     if (term === undefined) return pokemon;
 
     term = term.toString();
     if (term === "0") return pokemon;
+    
     // To match with pokemon's ID, we must add the good numbe of '0'. If just one digit, add two '0', if two digits only '0', and for more nothing to add.
     term = (term.length === 1) ? ('00' + term)
          : (term.length === 2) ? ('0' + term)
